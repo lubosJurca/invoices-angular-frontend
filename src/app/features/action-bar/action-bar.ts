3030e6c1b9ee21a9ab95c876bdcd3ba067bb34f1
Lubos Jurca <lubos.jurca@gmail.com>
2025-09-08 19:01:58 +0200

    working on filtering data request

--- src/app/features/action-bar/action-bar.ts
+++ src/app/features/action-bar/action-bar.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Data } from '../../shared/services/data';
+import { FormsModule } from '@angular/forms';
+import { Select } from 'primeng/select';
 
 @Component({
   selector: 'app-action-bar',
-  imports: [],
+  imports: [FormsModule, Select],
   templateUrl: './action-bar.html',
-  styleUrl: './action-bar.css'
+  styleUrl: './action-bar.css',
 })
 export class ActionBar {
+  private data = inject(Data);
+  public totalInvoices: number = 0;
+  public selectedStatus: string = 'all';
+  public statusOptions = new Array<{ label: string; value: string }>();
 
+  filterData() {
+    console.log('Filtering data by status:', this.selectedStatus);
+
+    this.data.getUserData(this.selectedStatus);
+  }
+
+  constructor() {
+    this.statusOptions = this.data.statusOptions;
+    this.data.currentInvoiceData$.subscribe((data) => {
+      if (data) {
+        this.totalInvoices = data.totalInvoices;
+      }
+    });
+  }
 }
--- src/app/shared/services/data.ts
+++ src/app/shared/services/data.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/enviroment';
 import { GetAllInvoicesResponse, User } from '../models/models';
-import { BehaviorSubject, catchError, EMPTY, tap } from 'rxjs';
+import { BehaviorSubject, catchError, EMPTY, takeUntil, tap } from 'rxjs';
 import { AuthService } from '../../core/auth/auth';
 
 @Injectable({
@@ -15,6 +15,13 @@ export class Data {
   private http = inject(HttpClient);
   private auth = inject(AuthService);
 
+  public statusOptions = [
+    { label: 'All', value: 'all' },
+    { label: 'Draft', value: 'draft' },
+    { label: 'Pending', value: 'pending' },
+    { label: 'Paid', value: 'paid' },
+  ];
+
   constructor() {
     this.auth.currentUser$.subscribe((user) => {
       if (user) {
@@ -24,11 +31,21 @@ export class Data {
       }
     });
   }
-  public getUserData(): void {
+
+  public getUserData(
+    status: string = 'all',
+    page: number = 1,
+    limit: number = 10,
+    skip: number = 1
+  ): void {
     this.http
-      .get<GetAllInvoicesResponse>(environment.apiUrl + '/invoices/', {
-        withCredentials: true,
-      })
+      .get<GetAllInvoicesResponse>(
+        environment.apiUrl +
+          `/invoices/?filter=${status}&page=${page}&limit=${limit}&skip=${skip}`,
+        {
+          withCredentials: true,
+        }
+      )
       .pipe(
         tap((response: GetAllInvoicesResponse) => {
           this.currentInvoiceData.next(response);
@@ -36,8 +53,13 @@ export class Data {
         catchError((error) => {
           console.error('Error fetching invoice data:', error);
           return EMPTY;
-        })
+        }),
+        takeUntil(this.auth.currentUser$.pipe(tap((user) => !user)))
       )
       .subscribe();
   }
+
+  // public getFilteredData(){ {
+  //   this.http.get<GetAllInvoicesResponse>(environment.apiUrl + '/invoices/?filter='+status, {withCredentials: true,})
+  // }
 }
